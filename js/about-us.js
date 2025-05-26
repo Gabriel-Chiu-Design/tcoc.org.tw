2ebe346c018ff1cfed6209f29c96c6df577a0394
Gabriel Chiu <118887862+Gabriel-Chiu-Design@users.noreply.github.com>
2025-05-26 21:36:34 +0800

    about us page update

--- js/about-us.js
+++ js/about-us.js
@@ -14,6 +14,22 @@ $(document).ready(function () {
     },
       });
 
+    lottie.loadAnimation({
+        container: document.getElementById('lottie-container'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: true,
+        path: 'https://raw.githubusercontent.com/Gabriel-Chiu-Design/tcoc.org.tw/refs/heads/main/element/Hero%20entry%20animation%20(1).json'
+    });
 
+    // ✅ Modify SVG after it loads
+    animation.addEventListener('DOMLoaded', () => {
+        const svg = document.querySelector('#lottie-container svg');
+        if (svg) {
+        svg.removeAttribute('width');
+        svg.removeAttribute('height');
+        svg.removeAttribute('style');
+        }
+    });
 
 });
